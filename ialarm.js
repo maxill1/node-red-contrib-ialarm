8b692115dcf3f1148114c13a86404ef11fd17937
Luca Mazzilli <setmaat@gmail.com>
2018-11-12 22:46:07 +0100

    set status and some docs

--- ialarm.js
+++ ialarm.js
@@ -21,6 +21,15 @@ module.exports = function(RED) {
     return st;
   }
 
+  function toValue(st){
+    for (var value in alarmStatus) {
+      if(alarmStatus[value] === st){
+        return value;
+      }
+    }
+    throw "unknown status";
+  }
+
   function decodeZoneMsg(ZoneMsg, i){
     var st = "OK";
     if(ZoneMsg[i-1] & 3){
@@ -60,16 +69,20 @@ module.exports = function(RED) {
     return zones;
   }
 
-  function connect(node, page, callback){
+  function getHeaders(serverConfig){
+    return {
+      'Authorization': 'Basic ' + Buffer.from(serverConfig.username + ':' + serverConfig.password).toString('base64')
+    };
+  }
+
+  function get(node, page, callback){
     const serverConfig = node.serverConfig;
     const url = 'http://'+serverConfig.host+':'+serverConfig.port+page;//'/SystemLog.htm';
     node.debug("Retrieving events: "+url);
     var auth = 'Basic ' + Buffer.from(serverConfig.username + ':' + serverConfig.password).toString('base64');
     var options = {
       url: url,
-      headers: {
-        'Authorization': auth
-      }
+      headers: getHeaders(serverConfig)
     };
     request.get(options, function(err, response, body){
       //console.log(JSON.stringify(response));
@@ -86,6 +99,29 @@ module.exports = function(RED) {
     });
   }
 
+  function post(node, page, formData, callback){
+    const serverConfig = node.serverConfig;
+    const url = 'http://'+serverConfig.host+':'+serverConfig.port+page;//'/SystemLog.htm';
+    node.debug("Retrieving events: "+url);
+    var options = {
+      url: url,
+      headers: getHeaders(serverConfig),
+      form: formData
+    };
+    request.post(options, function(err, response, body){
+      //console.log(JSON.stringify(response));
+      if(response.statusCode!==200 || err){
+        console.log('error '+response.statusCode+': '+err);
+        console.log(err);
+        node.status({fill:"red",shape:"dot",text:"Error: "+response.statusCode + " "+err});
+        node.send({payload: "Response error: "+response.statusCode});
+        return;
+      }
+      node.status({fill:"green",shape:"dot",text:"submitted"});
+
+      callback(body)
+    });
+  }
 
   function handleError(node, e){
     var msg = "error " +e.message;
@@ -109,7 +145,7 @@ module.exports = function(RED) {
 
     function fetchStatus() {
 
-      connect(node, '/RemoteCtr.htm' , function(body){
+      get(node, '/RemoteCtr.htm' , function(body){
         try {
           const $ = cheerio.load(body)
 
@@ -145,8 +181,7 @@ module.exports = function(RED) {
             }
           }
 
-          //node.log(JSON.stringify(data));
-          node.log("iAlarm status "+data.status);
+          //node.log("iAlarm status "+data.status);
           node.send({payload: data});
         } catch (e) {
           handleError(node, e);
@@ -173,7 +208,7 @@ module.exports = function(RED) {
 
     node.on('input', function(msg) {
 
-      connect(node, '/SystemLog.htm' , function(body){
+      get(node, '/SystemLog.htm' , function(body){
         try {
           const $ = cheerio.load(body)
           var events = [];
@@ -199,6 +234,55 @@ module.exports = function(RED) {
     });
   }
   RED.nodes.registerType("ialarm events", IalarmEvents);
+
+  function IalarmSet(config) {
+
+    var node = this;
+    //create node
+    RED.nodes.createNode(node, config);
+    //check and propagate configurations
+    node.serverConfig = RED.nodes.getNode(config.server);
+    if (!node.serverConfig) {
+      handleError(node, "invalid config");
+      return;
+    }
+
+    node.on('input', function(msg) {
+
+      var commandType = msg.payload;
+      var value = toValue(commandType);
+      var formData = {'Ctrl': value, 'BypassNum': '00', 'BypassOpt': '0'}
+
+      node.log("Sending " +JSON.stringify(formData));
+
+
+      post(node, '/RemoteCtr.htm', formData, function(body){
+        try {
+          const $ = cheerio.load(body)
+          var events = [];
+          $('tr').each(function(i, elem) {
+            var ev = $(this).html();
+
+            var child$ = cheerio.load(this);
+            let td = child$('td').map(function () {
+              return child$(this).text().trim();
+            }).get();
+
+            if(td[0] && td[1] && td[2]){
+              var event = {date: td[0], zone: td[1], message: td[2]};
+              events.push(event);
+            }
+          });
+          node.send({payload: events});
+
+        } catch (e) {
+          handleError(node, e);
+        }
+      });
+
+    });
+  }
+  RED.nodes.registerType("ialarm set", IalarmSet);
 
   function IalarmServerNode(config) {
     var node = this;
